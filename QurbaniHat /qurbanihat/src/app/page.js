17ded17fccc50e7a1c6e93df988f2f9ee0e9022d
Tanvirislam855e <ajtanvir855@gmail.com>
2026-04-30 22:36:43 +0600

    to generatoon edit

--- QurbaniHat /qurbanihat/src/app/page.js
+++ QurbaniHat /qurbanihat/src/app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Banner from "./components/Banner";
+import TopGenerations from "./components/TopGenerations";
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <Banner/>
+      <TopGenerations/>
     </div>
   );
 }
